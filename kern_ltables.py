dc6a9acb44cd063e7c148716ceb302c69a916638
Aswin Payyoor Vijayan <aswinpvijayan@gmail.com>
2019-08-28 11:02:12 +0100

    Corrected the way the kernel integral is calculated

--- kern_ltables.py
+++ kern_ltables.py
@@ -6,24 +6,22 @@ from functools import partial
 #All the calculations are in units of b/h (impact parameter/smoothing length).
 
 """
-    Follwing the same method used in `pyloser` (https://bitbucket.org/romeeld/pyloser/src/default/) 
-
-    Line of sight distance along a particle, l = sqrt(h^2 + b^2), where h and b 
+    Line of sight distance along a particle, l = 2*sqrt(h^2 + b^2), where h and b 
     are the smoothing length and the impact parameter respectively. This needs 
     to be weighted along with the kernel density function W(r), to calculate the 
-    los density. Integrated los density, D = 2 * integral(W(r)dl) from 0 to h, 
-    where r = sqrt(x^2 + b^2), W(r) is in units of f(r) h^-3 where f(r) is some 
-    function of r. 
+    los density. Integrated los density, D = 2 * integral(W(r)dz) from 0 to 
+    sqrt(h^2-b^2), where r = sqrt(z^2 + b^2), W(r) is in units of h^-3 
+    and is a function of r and h. 
 
     The parameters are normalized in terms of the smoothing length, helping us to 
     create a look-up table for every impact parameter along the line-of-sight.
     Hence we substitute x = x/h and b = b/h.  
-    dl = xdx/sqrt(x^2 + b^2) * h
     
     This implies
-    D = h^-2 * 2 * integral(f(r) dl) for x = 0 to 1.
+    D = h^-2 * 2 * integral(W(r) dz) for x = 0 to sqrt(1.-b^2).
 
-    The division by h^2 is to be done separately for each particle along the line-of-sight.
+    The division by h^2 is to be done separately for each particle along the 
+    line-of-sight.
 """
 
 kernsize = 10000
@@ -35,25 +33,25 @@ def inp_kernel(r, ktype):
         `r` is in units of h^-1 where h is the smoothing length of the object in consideration.
         The kernels are in units of h^-3, hence the need to divide by h^2 at the end.
         
-        Defined kernels at the moment are `uniform`, `anarchy`, `gadget-2`, `cubic`, `quintic`
+        Defined kernels at the moment are `uniform`, `sph-anarchy`, `gadget-2`, `cubic`, `quintic`
     """
     
     if ktype == 'uniform':
         
         if r < 1.:
-            return 1./((4./3.)*np.pi)
+            return 1./((4./3.)*pi)
         else:
             return 0.
     
-    elif ktype == 'anarchy':
+    elif ktype == 'sph-anarchy':
         
-        if r < 1.: return (21./(2.*pi)) * ((1. - r)*(1. - r)*(1. - r)*(1. - r)*(1. + 4.*r)) 
+        if r <= 1.: return (21./(2.*pi)) * ((1. - r)*(1. - r)*(1. - r)*(1. - r)*(1. + 4.*r)) 
         else: return 0.       
             
     elif ktype == 'gadget-2':
         
         if r < 0.5: return (8./pi) * (1. - 6*(r*r) + 6*(r*r*r))
-        elif r < 1.: return (8./pi) * ((1. - r)*(1. - r)*(1. - r))
+        elif r < 1.: return (8./pi) * 2 * ((1. - r)*(1. - r)*(1. - r))
         else: return 0.
     
     elif ktype == 'cubic':
@@ -71,28 +69,27 @@ def inp_kernel(r, ktype):
             
     else:
     
-        print ("Doesn't recognize the kernel. Input your own kernel in `inp_kernel` in `kern_ltables.py`")
+        print ("Doesn't recognize the kernel. Input your own kernel in `inp_kernel`")
         exit()
 
      
-def fr_dl(x, kern, b):
+def W_dz(z, kern, b):
     
     """
-        f(r)dl
+        W(r)dz
     """
     
-    ds = np.sqrt(b*b + x*x) 
+    return kern(np.sqrt(z**2 + b**2)) 
+
+def integral_func(kern, ii):
     
-    return (x/ds) * kern(ds) 
+    return lambda z : W_dz(z, kern, ii)
 
-def integral_func(kern, i):
-
-    return lambda x : fr_dl(x, kern, i)
 
 def get_kernel(ktype):
     
     """
-        h^-2 * 2 * integral(f(r) dl) from x = 0 to 1 for various values of `b`
+        h^-2 * 2 * integral(W(r) dz) from x = 0 to sqrt(1.-b^2) for various values of `b`
     """
     
     kernel = np.zeros(kernsize + 1)
@@ -101,27 +98,29 @@ def get_kernel(ktype):
     bins = np.arange(0, 1., 1./kernsize)
     bins = np.append(bins, 1.)
 
-    for i in range(kernsize):
+    for ii in range(kernsize):
 
-        y, yerr = integrate.quad(integral_func(this_kern, bins[i]), 0, 1.) 
-        kernel[i] = y * 2.
+        y, yerr = integrate.quad(integral_func(this_kern, bins[ii]), 0, np.sqrt(1.-bins[ii]**2))
+        kernel[ii] = y * 2.
         
     return kernel
     
-def create_kernel(ktype='anarchy'):
+def create_kernel(ktype='sph-anarchy'):
     
     """
         Saves the computed kernel for easy look-up as .npz file
     """
     
     kernel = get_kernel(ktype)
-    header = np.array(['kernel': ktype, 'bins': kernsize])
+    header = np.array([{'kernel': ktype, 'bins': kernsize}])
     np.savez('kernel_{}.npz'.format(ktype), header=header, kernel=kernel)
     
     print (header)
     
     return kernel
     
-    
+if __name__== "__main__": 
 
+    kernel =  create_kernel()   
+    print (kernel)
 

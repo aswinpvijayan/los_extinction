e48365ed161a7bba3dece24b679af95c14477dc0
Aswin Payyoor Vijayan <aswinpvijayan@gmail.com>
2019-10-03 11:08:21 +0100

    Added mpi version to speed up

--- mpi_extract_info.py
+++ mpi_extract_info.py
@@ -13,6 +13,7 @@ import eagle_IO as E
 from HDF5_write import HDF5_write
 from astropy.cosmology import Planck13 as cosmo
 import timeit
+from mpi4py import MPI
 
 norm = np.linalg.norm
 conv = (u.solMass/u.Mpc**2).to(u.solMass/u.pc**2)
@@ -165,11 +166,18 @@ def extract_info(num, tag, kernel='sph-anarchy', inp='GEAGLE'):
     
     """
     
+    ## MPI parameters
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    #print ("rank={}, size={}".format(rank, size))
+    
     kinp = np.load('kernel_{}.npz'.format(kernel), allow_pickle=True)
     lkernel = kinp['kernel']
     header = kinp['header']
     kbins = header.item()['bins']
-    print('\n\t\t\t###################################\nKernel used is `{}` and the number of bins in the look up table is {}\n\t\t\t###################################\n'.format(header.item()['kernel'], kbins))
+    if rank == 0:
+        print('\n\t\t\t###################################\nKernel used is `{}` and the number of bins in the look up table is {}\n\t\t\t###################################\n'.format(header.item()['kernel'], kbins))
     
     num = str(num)
     if inp == 'GEAGLE':
@@ -228,48 +236,55 @@ def extract_info(num, tag, kernel='sph-anarchy', inp='GEAGLE'):
 
     gc.collect()
 
-    print("Extracting required properties for {} subhalos from G-EAGLE_{} at z = {}".format(len(indices), num, z))
+    if rank == 0:
+        print("Extracting required properties for {} subhalos from G-EAGLE_{} at z = {}".format(len(indices), num, z))
     
-    snum = np.zeros(len(indices)+1).astype(int)
-    gnum = np.zeros(len(indices)+1).astype(int)
+    part = int(len(indices)/size)
+    comm.Barrier()
+
+    if rank!=size-1:
+        thisok = indices[rank*part:(rank+1)*part]
+    else:
+        thisok = indices[rank*part:]
+    
+    tsnum = np.zeros(len(thisok)+1).astype(int)
+    tgnum = np.zeros(len(thisok)+1).astype(int)
     
     sn = len(sp_mass)
     gn = len(gp_mass)
 
-    smass = np.empty(sn)
-    gmass = np.empty(gn)
+    tsmass = np.empty(sn)
+    tgmass = np.empty(gn)
     
-    sZ = np.empty(sn)
-    gZ = np.empty(gn)
+    tsZ = np.empty(sn)
+    tgZ = np.empty(gn)
     
-    scood = np.empty((sn,3))
-    gcood = np.empty((gn,3))
-    #sdist = np.empty((sn, len(indices)))
-    #gdist = np.empty((gn, len(indices)))
+    tscood = np.empty((sn,3))
+    tgcood = np.empty((gn,3))
     
-    s_sml = np.empty(sn) 
-    sage = np.empty(sn)
-    Z_los = np.empty(sn)
+    ts_sml = np.empty(sn) 
+    tg_sml = np.empty(gn) 
     
-    gc.collect()
-    
-    #sdist = cdist(sp_cood, cop[indices])
-    #gdist = cdist(gp_cood, cop[indices])
+    tsage = np.empty(sn)
+    tZ_los = np.empty(sn)
     
     gc.collect()
     
-    ind = np.array([])
+    ind = np.array([])    
     
     #Getting the indices (major) and the calculation of Z-LOS are the bottlenecks of this code. Maybe try 
     #cythonizing the Z-LOS part. Don't know how to change the logical operation part.
     kk = 0
-    for ii, jj in enumerate(indices):
+    
+    for ii, jj in enumerate(thisok):
+        
         start = timeit.default_timer()
         s_ok = np.where(np.logical_and(sp_sgrpn-sgrpno[jj]==0,sp_grpn-grpno[jj]==0))[0]
         s_ok = s_ok[norm(sp_cood[s_ok]-cop[jj],axis=1)<=0.03]
         g_ok = np.where(np.logical_and(gp_sgrpn-sgrpno[jj]==0,gp_grpn-grpno[jj]==0))[0] 
         g_ok = g_ok[norm(gp_cood[g_ok]-cop[jj],axis=1)<=0.03]
         stop = timeit.default_timer()
+        
         if len(s_ok) + len(g_ok) >= 100:
         
             print ("Calculating indices took {}s".format(np.round(stop - start,6)))
@@ -281,28 +296,30 @@ def extract_info(num, tag, kernel='sph-anarchy', inp='GEAGLE'):
             print ("Calculating Z_los took {}s".format(np.round(stop - start,6)))
             
             start = timeit.default_timer()
-            snum[kk+1] = len(s_ok)
-            gnum[kk+1] = len(g_ok)
+            tsnum[kk+1] = len(s_ok)
+            tgnum[kk+1] = len(g_ok)
             
-            scum = np.cumsum(snum)
-            gcum = np.cumsum(gnum)            
+            scum = np.cumsum(tsnum)
+            gcum = np.cumsum(tgnum)            
             sbeg = scum[kk]
             send = scum[kk+1]
             gbeg = gcum[kk]
             gend = gcum[kk+1]
             
-            scood[sbeg:send] = sp_cood[s_ok]
-            gcood[gbeg:gend] = gp_cood[g_ok]
+            tscood[sbeg:send] = sp_cood[s_ok]
+            tgcood[gbeg:gend] = gp_cood[g_ok]
             
-            smass[sbeg:send] = sp_mass[s_ok]
-            gmass[gbeg:gend] = gp_mass[g_ok]
+            tsmass[sbeg:send] = sp_mass[s_ok]
+            tgmass[gbeg:gend] = gp_mass[g_ok]
             
-            sZ[sbeg:send] = sp_Z[s_ok]
-            gZ[gbeg:gend] = gp_Z[g_ok]
+            tsZ[sbeg:send] = sp_Z[s_ok]
+            tgZ[gbeg:gend] = gp_Z[g_ok]
             
-            s_sml[sbeg:send] = sp_sl[s_ok]
-            sage[sbeg:send] = sp_ft[s_ok]
-            Z_los[sbeg:send] = Z_los_SD
+            ts_sml[sbeg:send] = sp_sl[s_ok]
+            tg_sml[gbeg:gend] = gp_sl[g_ok]
+            
+            tsage[sbeg:send] = sp_ft[s_ok]
+            tZ_los[sbeg:send] = Z_los_SD
             stop = timeit.default_timer()
             print ("Assigning arrays took {}s".format(np.round(stop - start,6)))
             gc.collect()
@@ -310,20 +327,82 @@ def extract_info(num, tag, kernel='sph-anarchy', inp='GEAGLE'):
         else:
         
             ind = np.append(ind, ii)
-            
+       
     ##End of loop ii, jj##
-    indices = np.delete(indices, ind.astype(int))
-    del sgrpno, grpno, sp_sgrpn, sp_grpn, sp_mass, sp_Z, sp_cood, sp_sl, sp_ft, gp_sgrpn, gp_grpn, gp_mass, gp_Z, gp_cood, gp_sl, ind 
-   
-    gc.collect()
-    
-    stot = np.sum(snum)
-    gtot = np.sum(gnum)
-    
-    sage = get_age(sage[:stot], z, 4)
+    del sgrpno, grpno, sp_sgrpn, sp_grpn, sp_mass, sp_Z, sp_cood, sp_sl, sp_ft, gp_sgrpn, gp_grpn, gp_mass, gp_Z, gp_cood, gp_sl
 
-    return mstar[indices], cop[indices], snum[1:len(indices)+1], gnum[1:len(indices)+1], scood[:stot], gcood[:gtot], smass[:stot], gmass[:gtot], sZ[:stot], gZ[:gtot], s_sml[:stot], sage, Z_los[:stot]
+    gc.collect()
+
     
+    thisok = np.delete(thisok, ind.astype(int))    
+    
+    tstot = np.sum(tsnum)
+    tgtot = np.sum(tgnum)
+    
+    tsnum = tsnum[1:len(thisok)+1]
+    tgnum = tgnum[1:len(thisok)+1]
+    
+    tscood = tscood[:tstot]
+    tgcood = tgcood[:tgtot]
+    
+    tsmass = tsmass[:tstot] 
+    tgmass = tgmass[:tgtot] 
+    
+    tsZ = tsZ[:tstot] 
+    tgZ = tgZ[:tgtot] 
+    
+    ts_sml = ts_sml[:tstot]
+    tg_sml = tg_sml[:tgtot]
+    
+    tsage = get_age(tsage[:tstot], z, 4)
+    tZ_los = tZ_los[:tstot]
+    
+    comm.Barrier()
+    
+    if rank == 0:
+        
+        print ("Gathering data from different processes")
+    
+    indices = comm.gather(thisok, root=0)
+    snum = comm.gather(tsnum, root=0)
+    gnum = comm.gather(tgnum, root=0)
+    scood = comm.gather(tscood, root=0)
+    gcood = comm.gather(tgcood, root=0)
+    smass = comm.gather(tsmass, root=0)
+    gmass = comm.gather(tgmass, root=0)
+    sZ = comm.gather(tsZ, root=0)
+    gZ = comm.gather(tgZ, root=0)
+    s_sml = comm.gather(ts_sml, root=0)
+    g_sml = comm.gather(tg_sml, root=0)
+    sage = comm.gather(tsage, root=0)
+    Z_los = comm.gather(tZ_los, root=0)
+    
+    
+    if rank == 0:
+        
+        print ("Gathering completed")    
+        indices = np.concatenate(np.array(indices))
+        snum = np.concatenate(np.array(snum))
+        gnum = np.concatenate(np.array(gnum))
+        
+        scood = np.concatenate(np.array(scood), axis = 0)  
+        gcood = np.concatenate(np.array(gcood), axis = 0)  
+        
+        smass = np.concatenate(np.array(smass))
+        gmass = np.concatenate(np.array(gmass))
+        
+        sZ = np.concatenate(np.array(sZ))
+        gZ = np.concatenate(np.array(gZ))
+        
+        s_sml = np.concatenate(np.array(s_sml))
+        g_sml = np.concatenate(np.array(g_sml))
+        
+        sage = np.concatenate(np.array(sage))
+        Z_los = np.concatenate(np.array(Z_los))
+    
+   
+    return mstar[indices], cop[indices], snum, gnum, scood, gcood, smass, gmass, sZ, gZ, s_sml, g_sml, sage, Z_los
+
 ##End of function `extract_info`        
 
 def save_to_hdf5(num, tag, kernel='sph-anarchy', inp='GEAGLE'):
@@ -338,34 +417,48 @@ def save_to_hdf5(num, tag, kernel='sph-anarchy', inp='GEAGLE'):
         sim = ['/cosma5/data/Eagle/ScienceRuns/Planck1/L0100N1504/PE/REFERENCE/data', '/cosma5/data/Eagle/ScienceRuns/Planck1/L0050N0752/PE/AGNdT9/data/'][inp]
         filename = 'data2/EAGLE_{}_sp_info.hdf5'.format(['REF', 'AGNdT9'][inp])
     
-    mstar, cop, snum, gnum, scood, gcood, smass, gmass, sZ, gZ, s_sml, sage, Z_los = extract_info(num, tag, kernel, inp)
+    ## MPI parameters
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    
+    if rank == 0:
+    
+        print ("#################    Saving required properties to hdf5     #############")
+        print ("#################   Number of processors being used is {}   #############".format(size))
+    
+    
+    mstar, cop, snum, gnum, scood, gcood, smass, gmass, sZ, gZ, s_sml, g_sml, sage, Z_los = extract_info(num, tag, kernel, inp)
 
+    if rank == 0:
     
-    print("Wrting out required properties to hdf5")
-    
-    hdf5_store = HDF5_write(filename)
-    hdf5_store.create_grp(tag)
+        print("Wrting out required properties to hdf5")
+        
+        hdf5_store = HDF5_write(filename)
+        hdf5_store.create_grp(tag)
 
-    hdf5_store.create_grp('{}/Subhalo'.format(tag))
-    hdf5_store.create_grp('{}/Particle'.format(tag))
-    
+        hdf5_store.create_grp('{}/Subhalo'.format(tag))
+        hdf5_store.create_grp('{}/Particle'.format(tag))
+        
 
-    hdf5_store.create_dset(mstar, 'Mstar_30', '{}/Subhalo'.format(tag))
-    hdf5_store.create_dset(cop.T, 'COP', '{}/Subhalo'.format(tag))
+        hdf5_store.create_dset(mstar, 'Mstar_30', '{}/Subhalo'.format(tag))
+        hdf5_store.create_dset(cop.T, 'COP', '{}/Subhalo'.format(tag))
 
-    hdf5_store.create_dset(snum, 'S_Length', '{}/Subhalo'.format(tag), dtype = np.int64)
-    hdf5_store.create_dset(gnum, 'G_Length', '{}/Subhalo'.format(tag), dtype = np.int64)
-    
-    hdf5_store.create_dset(scood.T, 'S_Coordinates', '{}/Particle'.format(tag))
-    hdf5_store.create_dset(gcood.T, 'G_Coordinates', '{}/Particle'.format(tag))
-            
-    hdf5_store.create_dset(smass, 'S_Mass', '{}/Particle'.format(tag))
-    hdf5_store.create_dset(gmass, 'G_Mass', '{}/Particle'.format(tag))
-    
-    hdf5_store.create_dset(sZ, 'S_Z', '{}/Particle'.format(tag))
-    hdf5_store.create_dset(gZ, 'G_Z', '{}/Particle'.format(tag))
-    
-    hdf5_store.create_dset(s_sml, 'S_sml', '{}/Particle'.format(tag))
-    hdf5_store.create_dset(sage, 'S_Age', '{}/Particle'.format(tag))
-    hdf5_store.create_dset(Z_los, 'S_los', '{}/Particle'.format(tag))     
-    
+        hdf5_store.create_dset(snum, 'S_Length', '{}/Subhalo'.format(tag), dtype = np.int64)
+        hdf5_store.create_dset(gnum, 'G_Length', '{}/Subhalo'.format(tag), dtype = np.int64)
+        
+        hdf5_store.create_dset(scood.T, 'S_Coordinates', '{}/Particle'.format(tag))
+        hdf5_store.create_dset(gcood.T, 'G_Coordinates', '{}/Particle'.format(tag))
+                
+        hdf5_store.create_dset(smass, 'S_Mass', '{}/Particle'.format(tag))
+        hdf5_store.create_dset(gmass, 'G_Mass', '{}/Particle'.format(tag))
+        
+        hdf5_store.create_dset(sZ, 'S_Z', '{}/Particle'.format(tag))
+        hdf5_store.create_dset(gZ, 'G_Z', '{}/Particle'.format(tag))
+        
+        hdf5_store.create_dset(s_sml, 'S_sml', '{}/Particle'.format(tag))
+        hdf5_store.create_dset(g_sml, 'G_sml', '{}/Particle'.format(tag))
+        
+        hdf5_store.create_dset(sage, 'S_Age', '{}/Particle'.format(tag))
+        hdf5_store.create_dset(Z_los, 'S_los', '{}/Particle'.format(tag))     
+
